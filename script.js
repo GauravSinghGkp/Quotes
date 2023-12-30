7a2b78de0a9e8748fea1df648a3655a639972870
Gaurav Singh <Gaurav1998up@gmail.com>
2023-12-30 15:59:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const quotes = document.getElementById("quote");
+const dropdown = document.getElementById("topics");
+const quoteText = document.getElementById("quote");
+const authorName = document.getElementById("author");
+const btn = document.getElementById("button");
+
+let selectedCategory = "humor";
+
+generateNewQuote();
+
+dropdown.addEventListener("change", () => {
+  selectedCategory = dropdown.value;
+});
+
+btn.addEventListener("click", generateNewQuote);
+
+function generateNewQuote() {
+  fetch(`https://api.api-ninjas.com/v1/quotes?category=${selectedCategory}`, {
+    headers: {
+      "X-Api-Key": "0sVRtMX15v0The4V4zzA9A==mNM0aoM0M2T54UPf",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      displayQuote(data[0].quote, data[0].author);
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
+}
+
+function displayQuote(quote, author) {
+  quoteText.innerText = quote;
+  authorName.innerText = author;
+}
